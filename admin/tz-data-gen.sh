517bdb89e453031769d8d0d0c81a586545ca1925
Sakari Poussa <spoussa@gmail.com>
2018-05-03 19:54:29 -0700

    New file to generate TZ record

---
+++ admin/tz-data-gen.sh
@@ -0,0 +1,21 @@
+var moment = require('moment-timezone');
+require('moment-timezone/moment-timezone-utils')
+
+const tz_name = "Europe/Helsinki";
+
+var zones = [];
+let zone = moment.tz.zone(tz_name)
+
+// For tz.add(...)
+var subset = moment.tz.filterYears(zone, 2018);
+console.log("\"" + moment.tz.pack(subset) + "\"")
+
+// For tz.load(...)
+zones.push(zone)
+let input = {}
+input.version = "2018"
+input.zones = zones;
+input.links = [];
+
+let pack = moment.tz.filterLinkPack(input, 2018, 2018)
+console.log(JSON.stringify(pack));
